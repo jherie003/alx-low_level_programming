0a9f7b25d7a27155bdbcd416c259923f3fa7dd03
jherie003 <jherie@alxstudent.com>
2023-03-19 02:26:20 -0700

    all

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,7 @@
  * return: 1
  */
 int main(void)
-{ write(1, "and that piece of art is useful\" - dora korpar, 2015-10-19\n" 59);
+{
+       	write(1, "and that piece of art is useful\" - dora korpar, 2015-10-19\n" 59);
 	return(1);
 }
